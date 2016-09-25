26b5166c6e0d7cdc6a174a8c74cfccc3dff556fb
Ashraf <rootkali95@gmail.com>
2016-09-26 03:14:11 +0800

    update der

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -62,7 +62,7 @@
                                     Logout
                                 </a>
 
-                                <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
+                                <form id="logout-form" action="{{ url('/auth/logout') }}" method="POST" style="display: none;">
                                     {{ csrf_field() }}
                                 </form>
                             </li>
