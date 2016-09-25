e2250d9cede70626677e796833ac93be312b5198
aminuddinz <aminblass93@hotmail.com>
2016-09-26 00:04:14 +0800

    first commit

---
+++ resources/views/partials/admin_controls/privacy_switcher.blade.php
@@ -0,0 +1,12 @@
+@if(Auth::user()->isAdmin())
+
+    <a href="javascript:void(0)" v-on:click="showPrivate()" v-show="!showPrivateMessages">
+        <i class="fa fa-eye text-success"></i>
+        Show private messages
+    </a>
+    <a href="javascript:void(0)" v-on:click="hidePrivate()" v-show="showPrivateMessages">
+        <i class="fa fa-eye-slash text-danger"></i>
+        Hide private messages
+    </a>
+
+@endif
